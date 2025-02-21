dcf6a24ca5fdefa6980a72ffefc77414316f1a99
freeayoub <gayoub2009@gmail.com>
2025-02-21 22:18:57 +0100

    first commit

--- firstproject/src/logement.ts
+++
@@ -1,9 +0,0 @@
-export interface Logement {
-  reference: number;
-  adresse: string;
-  delegation: string;
-  gouvernorat: string;
-  type: string;
-  description: string;
-  prix: number;
-}
--- firstproject/src/rendezvous.ts
+++
@@ -1,9 +0,0 @@
-import { Logement } from "./logement";
-
-export interface Rendezvous {
-  id: number;
-  date: string;
-  heure: string;
-  logement: Logement; 
-  numTel: string;
-}
